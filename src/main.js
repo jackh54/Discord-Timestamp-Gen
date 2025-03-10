24f99c8efd82d9cf043004e985dcec42d9593f2e
Jack <jackh.224307@gmail.com>
2025-03-09 20:51:53 -0500

    remove speed insights

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
 import { createApp } from 'vue'
-import { inject } from '@vercel/analytics'
-import { injectSpeedInsights } from '@vercel/speed-insights/vue'
 import App from './App.vue'
 import ToastPlugin from 'vue-toast-notification'
 import 'vue-toast-notification/dist/theme-sugar.css'
 
 const app = createApp(App)
 app.use(ToastPlugin)
-app.mount('#app')
-
-// Initialize Vercel Speed Insights
-injectSpeedInsights()
-// Initialize Vercel Analytics
-inject() 
+app.mount('#app') 
