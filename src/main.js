42b221ae8d51d8e9050a771a3aa2ffcc900a2ea2
Jack <jackh.224307@gmail.com>
2025-03-09 20:44:38 -0500

    add speed insights

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
+import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights/vue'
 import App from './App.vue'
 import ToastPlugin from 'vue-toast-notification'
 import 'vue-toast-notification/dist/theme-sugar.css'
 
 const app = createApp(App)
 app.use(ToastPlugin)
-app.mount('#app') 
+app.mount('#app')
+
+// Initialize Vercel Speed Insights
+injectSpeedInsights()
+// Initialize Vercel Analytics
+inject() 
